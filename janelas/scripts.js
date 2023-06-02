9e8e8a130a9db73afd2a16703b7c28939bc92a1b
João Paulo <125047858+joaopaulo0611@users.noreply.github.com>
2023-06-02 12:39:53 -0300

    Add files via upload

---
+++ janelas/scripts.js
@@ -0,0 +1,24 @@
+function converte(){
+    var temperaturainicial = Number(document.getElementById("campo").value)
+    var resultado = 0
+
+    if(document.getElementById("cf").checked){
+         resultado = (temperaturainicial * 9/5) +32
+       }
+       else if(document.getElementById("fc").checked){
+      resultado = (temperaturainicial - 32) * 5/9
+       }
+       else if(document.getElementById("ck").checked){
+          resultado = (temperaturainicial + 273,15)
+       }
+       else if(document.getElementById("kc").checked){
+          resultado = (temperaturainicial - 273,15)
+       }
+       else if(document.getElementById("kf").checked){
+          resultado = (temperaturainicial - 273,15) * (9/5) + 32
+       }
+       else if(document.getElementById("fk").checked){
+          resultado = (temperaturainicial - 32) * (5/9) + 273,15
+       }
+          document.getElementById("r").innerHTML = resultado 
+     }
